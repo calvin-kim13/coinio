e459ad35e5eb8a62919c64fa419f8515966fc220
Calvin Kim <calvinkim1230@gmail.com>
2022-04-02 06:14:46 -0700

    added hero section/header to the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Coinio</h1>
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
