e118e462a2e542dc055258759079bee0f4f8b21d
Calvin Kim <calvinkim1230@gmail.com>
2022-04-02 06:34:46 -0700

    added search bar and crypto table header

---
+++ src/components/CoinsTable.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { CoinList } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import {
+  Container,
+  createTheme,
+  ThemeProvider,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+
+const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState();
+
+  const { currency } = CryptoState();
+
+  const fetchCoins = async () => {
+    setLoading(true);
+
+    const { data } = await axios.get(CoinList(currency));
+
+    setCoins(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, [currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          style={{ margin: 18, fontFamily: "Montserrat" }}
+        >
+          Cryptocurrencies by Market Cap Rank
+        </Typography>
+        <TextField
+          label="Search For a Crypto Currency.."
+          variant="outlined"
+          style={{ marginBottom: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default CoinsTable;
